45b68cf989703377de3ec123a88e80206dd895de
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-03-19 15:56:03 -0500

    updated routes

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { 
-    createLoan, 
-    getAllLoans, 
-    getLoanById,
-    updateLoan,
-    deleteLoan
+    getAllResources, 
+    getResourceById, 
+    createResource,
+    updateResource,
+    deleteResource
 } from '../controllers/resourceController';
 import authenticate from "../middleware/authenticate";
 import authorize from "../middleware/authorize";
@@ -12,10 +12,10 @@ import authorize from "../middleware/authorize";
 
 const router: Router = Router();
 
-router.get('/', authenticate, authorize({hasRole:["admin", "manager", "officer"], allowSameUser: true}), getAllLoans);
-router.get('/:id', authenticate, authorize({hasRole:["admin", "manager", "officer"], allowSameUser: true}), getLoanById);
-router.post('/', authenticate, authorize({hasRole:["admin", "manager"], allowSameUser: true}), createLoan);
-router.put('/:id', authenticate, authorize({hasRole:["admin", "manager"], allowSameUser: true}), updateLoan);
-router.delete('/:id', authenticate, authorize({hasRole:["admin"], allowSameUser: true}), deleteLoan);
+router.get('/', authenticate, authorize({hasRole:["admin", "manager", "officer"], allowSameUser: true}), getAllResources);
+router.get('/:id', authenticate, authorize({hasRole:["admin", "manager", "officer"], allowSameUser: true}), getResourceById);
+router.post('/', authenticate, authorize({hasRole:["admin", "manager"], allowSameUser: true}), createResource);
+router.put('/:id', authenticate, authorize({hasRole:["admin", "manager"], allowSameUser: true}), updateResource);
+router.delete('/:id', authenticate, authorize({hasRole:["admin"], allowSameUser: true}), deleteResource);
 
 export default router;
